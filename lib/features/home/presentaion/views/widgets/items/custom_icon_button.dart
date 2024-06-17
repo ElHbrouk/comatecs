a96d164e3e3e03967432a2683961832a73374f48
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-17 14:58:15 +0300

    Refactor CustomIconButton widget
    - Add import statement for AppColors
    - Add color parameter to customize button background color
    - Set button background color based on color parameter
    - Set button shape to circular with a radius of 16
    - Update icon size to 20

--- lib/features/home/presentaion/views/widgets/items/custom_icon_button.dart
+++ lib/features/home/presentaion/views/widgets/items/custom_icon_button.dart
@@ -1,12 +1,25 @@
+import 'package:comatecs/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomIconButton extends StatelessWidget {
-  const CustomIconButton({super.key, required this.icon, this.onPressed});
+  const CustomIconButton(
+      {super.key, required this.icon, this.onPressed, required this.color});
   final IconData icon;
   final void Function()? onPressed;
+  final bool color;
   @override
   Widget build(BuildContext context) {
     return IconButton(
+      style: IconButton.styleFrom(
+        backgroundColor: color == true
+            ? AppColors.primaryColor.withOpacity(0.4)
+            : Colors.grey.withOpacity(0.2),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
+        ),
+      ),
       iconSize: 20,
       onPressed: onPressed,
       icon: Icon(

240af1ffa071723cb42284c289c6459940e934b6
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-21 15:02:32 +0300

    create account setting item and editing account view

--- lib/features/account/presentation/account_view.dart
+++ lib/features/account/presentation/account_view.dart
@@ -1,3 +1,4 @@
+import 'package:comatecs/features/account/presentation/widgets/account_view_body.dart';
 import 'package:flutter/material.dart';
 
 class AccountView extends StatelessWidget {
@@ -5,56 +6,6 @@ class AccountView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('عمر محمد'),
-        const Text('Omar.M@gmail.com'),
-        Container(
-          margin: const EdgeInsets.symmetric(
-            vertical: 10.0,
-          ),
-          alignment: Alignment.center,
-          height: 40,
-          width: 90,
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: const Text('500 نقطة'),
-        ),
-        ListView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 5,
-          shrinkWrap: true,
-          itemBuilder: (context, index) => GestureDetector(
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
-                child: Row(
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.edit),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('تعديل الملف الشخصي'),
-                      ],
-                    ),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
+    return const AccountViewBody();
   }
 }

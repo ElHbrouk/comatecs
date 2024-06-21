fb80f156045c37d25f3283e571d2df708a188a08
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-21 15:03:55 +0300

    create edit profile view

---
+++ lib/features/account/presentation/edit_profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:comatecs/features/account/presentation/widgets/edit_profile_view_body.dart';
+import 'package:flutter/material.dart';
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const EditProfileViewBody(),
+    );
+  }
+}

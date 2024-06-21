3f5b3c866e9385fcdbcb9f2acde8e6ba748b14dd
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-21 15:12:26 +0300

    adding go router

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -1,5 +1,7 @@
 import 'package:comatecs/bindings/signup_bindings.dart';
 import 'package:comatecs/core/middlewares/on_boarding_miiddleware.dart';
+import 'package:comatecs/features/account/presentation/account_view.dart';
+import 'package:comatecs/features/account/presentation/edit_profile_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/forget_password/check_email_password_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/forget_password/reset_password_success_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/forget_password/reset_password_view.dart';
@@ -8,19 +10,22 @@ import 'package:comatecs/features/auth/presentaion/views/login/login_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/sign_up/sign_up_success_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/sign_up/sign_up_view.dart';
 import 'package:comatecs/features/auth/presentaion/views/sign_up/verify_code_sign_up_view.dart';
+import 'package:comatecs/features/cart/presentaion/view/cart_view.dart';
+import 'package:comatecs/features/favourite/presentaion/view/favourite_view.dart';
 import 'package:comatecs/features/home/presentaion/views/home_view.dart';
+import 'package:comatecs/features/home/presentaion/views/item_detail_view.dart';
 import 'package:comatecs/features/language/presentation/views/language_view.dart';
 import 'package:comatecs/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:comatecs/features/splash/presentaion/views/splash_view.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 abstract class AppRoutes {
-  static const mainView = "/";
-  static const homeView = "/homeView";
+  static const splashView = "/";
   static const languageView = "/languageView";
-  // onBoarding
+  // --------------- onBoarding views --------------
   static const onBoardingView = "/onBoardingView";
-  // Auth
+  // --------------- Auth views ---------------
   static const loginView = "/loginView";
   static const signUpView = "/signUpView";
   static const forgetPasswordView = "/forgetPasswordView";
@@ -30,61 +35,87 @@ abstract class AppRoutes {
   static const signUpSuccess = "/signUpSuccess";
   static const verifyCodeSignUpView = "/verifyCodeSignUpView";
 
-  static List<GetPage<dynamic>>? routes = [
-    GetPage(
-      name: "/",
-      page: () => const SplashView(),
+//--------------- items views ---------------
+  static const homeView = "/homeView";
+  static const itemDetailView = "/itemDetailView";
+  static const accountView = "/accountView";
+  static const cartView = "/cartView";
+  static const favouriteView = "/favouriteView";
+  static const editProfileView = "/editProfileView";
+
+  static GoRouter routes = GoRouter(routes: [
+    GoRoute(
+      path: splashView,
+      builder: (context, state) => const SplashView(),
     ),
-    GetPage(
-      name: loginView,
-      page: () => const LoginView(),
+    GoRoute(
+      path: loginView,
+      builder: (context, state) => const LoginView(),
     ),
-    GetPage(
-      middlewares: [
-        OnBoardingMiddleware(),
-      ],
-      name: languageView,
-      page: () => const Language(),
+    // GoRoute(
+    //   path: languageView,
+    //   builder: (context, state) => const Language(),
+    // ),
+    GoRoute(
+      // middlewares: [
+      //   OnBoardingMiddleware(),
+      // ],
+      path: onBoardingView,
+      builder: (context, state) => const OnBoardingView(),
     ),
-    GetPage(
-      middlewares: [
-        OnBoardingMiddleware(),
-      ],
-      name: onBoardingView,
-      page: () => const OnBoardingView(),
+    GoRoute(
+      // binding: SignUpBindings(),
+      path: signUpView,
+      builder: (context, state) => const SignUpView(),
     ),
-    GetPage(
-      binding: SignUpBindings(),
-      name: signUpView,
-      page: () => const SignUpView(),
+    GoRoute(
+      path: forgetPasswordView,
+      builder: (context, state) => const ForgetPasswordView(),
     ),
-    GetPage(
-      name: forgetPasswordView,
-      page: () => const ForgetPasswordView(),
+    GoRoute(
+      path: resetPasswordView,
+      builder: (context, state) => const ResetPasswordView(),
     ),
-    GetPage(
-      name: resetPasswordView,
-      page: () => const ResetPasswordView(),
+    GoRoute(
+      path: resetPasswordSuccessView,
+      builder: (context, state) => const ResetPasswordSuccess(),
     ),
-    GetPage(
-      name: resetPasswordSuccessView,
-      page: () => const ResetPasswordSuccess(),
+    GoRoute(
+      path: homeView,
+      builder: (context, state) => const HomeView(),
     ),
-    GetPage(
-      name: homeView,
-      page: () => const HomeView(),
+    GoRoute(
+      path: verifyCodePasswordView,
+      builder: (context, state) => const VerifyCodePasswordView(),
     ),
-    GetPage(
-      name: verifyCodePasswordView,
-      page: () => const VerifyCodePasswordView(),
+    GoRoute(
+      path: signUpSuccess,
+      builder: (context, state) => const SignUpSuccess(),
     ),
-    GetPage(
-      name: signUpSuccess,
-      page: () => const SignUpSuccess(),
+    GoRoute(
+      path: verifyCodeSignUpView,
+      builder: (context, state) => const VerifyCodeSignUpView(),
     ),
-    GetPage(
-      name: verifyCodeSignUpView,
-      page: () => const VerifyCodeSignUpView(),
+    GoRoute(
+      path: itemDetailView,
+      builder: (context, state) => const ItemDetailView(),
     ),
-  ];
+    GoRoute(
+      path: accountView,
+      builder: (context,state) => const AccountView(),
+    ),
+    GoRoute(
+      path: cartView,
+      builder: (context,state) => const CartView(),
+    ),
+    GoRoute(
+
+      path: favouriteView,
+      builder: (context,state) => const FavouriteView(),
+    ),
+    GoRoute(
+      path: editProfileView,
+      builder: (context,state) => const EditProfileView(),
+    ),
+  ]);
 }

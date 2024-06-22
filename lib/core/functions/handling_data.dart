5338177672deff9a43aeb864fa1d820d608bd1e3
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-22 19:18:28 +0300

    Add EditAddressView widget
    This commit adds a new widget called EditAddressView which displays the EditAddressViewBody widget. The EditAddressView widget is used to edit user addresses in the account section of the app.
    and removing controllers

--- lib/core/functions/handling_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:comatecs/core/class/statues_request.dart';
-
-handlingData(response) {
-  if (response is StatuesRequest) {
-    return response;
-  } else {
-    return StatuesRequest.success;
-  }
-}

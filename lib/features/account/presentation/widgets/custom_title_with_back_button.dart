97535a4a78f48312e53c94d995dea5070f0ef63f
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-22 11:27:52 +0300

    Add CustomTitleWithBackButton widget to account feature presentation widgets.

---
+++ lib/features/account/presentation/widgets/custom_title_with_back_button.dart
@@ -0,0 +1,25 @@
+import 'package:comatecs/core/shared/widgets/custom_back_button.dart';
+import 'package:comatecs/core/utils/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+class CustomTitleWithBackButton extends StatelessWidget {
+  const CustomTitleWithBackButton({
+    super.key, required this.title,
+  });
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const CustomBackButton(),
+        Text(
+          title,
+          style: AppFonts.bold16.copyWith(
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}

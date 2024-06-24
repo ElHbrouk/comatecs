8458c1b2617da7e9077b9973e85f81b4bd7dd4b1
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-24 07:33:57 +0300

    Add MyOrdersViewBody widget
    Added a new widget called MyOrdersViewBody which contains a CustomViewTitle and CustomTabBar to display the user's orders.

---
+++ lib/features/my_orders/presentation/widgets/my_order_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:comatecs/core/shared/widgets/custom_view_title.dart';
+import 'package:comatecs/features/my_orders/presentation/widgets/custom_tab_bar.dart';
+import 'package:flutter/material.dart';
+
+class MyOrdersViewBody extends StatelessWidget {
+  const MyOrdersViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: CustomViewTitle(
+            title: 'طلباتي',
+          ),
+        ),
+        Expanded(
+          child: CustomTabBar(),
+        ),
+        
+      ],
+    );
+  }
+}

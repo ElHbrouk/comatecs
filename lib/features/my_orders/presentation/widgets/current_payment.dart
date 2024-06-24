d0edd08158ff00049916c57b63526b897a51e61a
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-06-24 07:33:48 +0300

    Add tab bar item views for current, completed, and cancelled payments
    This commit adds three new widgets for displaying payment items in the
    corresponding tab bar items: CurrentPayment, CompletedPayment, and
    CancelledPayment. Each widget contains a list of PaymentItemModel and
    displays them using the TabBarItemListView widget. The payment status and
    order status colors are customized for each widget.

---
+++ lib/features/my_orders/presentation/widgets/current_payment.dart
@@ -0,0 +1,53 @@
+import 'package:comatecs/features/my_orders/data/payment_item_model.dart';
+import 'package:comatecs/features/my_orders/presentation/widgets/tab_bar_item_list_view.dart';
+import 'package:flutter/material.dart';
+
+class CurrentPayment extends StatelessWidget {
+  const CurrentPayment({super.key});
+  final List<PaymentItemModel> paymentItemModel = const [
+    PaymentItemModel(
+      id: '#1254631',
+      date: '20/9/2023',
+      paymentStatus: 'غير مدفوعة',
+      orderStatus: 'قيد المراجعة',
+      price: '16.000',
+    ),
+    PaymentItemModel(
+      id: '#1254631',
+      date: '20/9/2023',
+      paymentStatus: 'غير مدفوعة',
+      orderStatus: 'قيد المراجعة',
+      price: '16.000',
+    ),
+    PaymentItemModel(
+      id: '#1254631',
+      date: '20/9/2023',
+      paymentStatus: 'غير مدفوعة',
+      orderStatus: 'قيد المراجعة',
+      price: '16.000',
+    ),
+    PaymentItemModel(
+      id: '#1254631',
+      date: '20/9/2023',
+      paymentStatus: 'غير مدفوعة',
+      orderStatus: 'قيد المراجعة',
+      price: '16.000',
+    ),
+    PaymentItemModel(
+      id: '#1254631',
+      date: '20/9/2023',
+      paymentStatus: 'غير مدفوعة',
+      orderStatus: 'قيد المراجعة',
+      price: '16.000',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarItemListView(
+      paymentItemModel: paymentItemModel,
+      orderStatus: Colors.grey,
+      paymentStatus: Colors.red,
+    );
+  }
+}

bb6fbf052e4b3ba3905bef5b73d4a6a6c16ff362
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-09-04 15:52:05 +0300

    Add ApiService class for handling HTTP requests
    This commit introduces the `ApiService` class, a utility designed to facilitate HTTP requests within the application. The class is built on top of the `Dio` package, providing a robust interface for making GET, POST, and PATCH requests to a specified API endpoint. It includes configurable options for headers and content type, ensuring flexibility and ease of use across different API interactions. The implementation adheres to best practices for handling asynchronous network operations, returning parsed JSON data for GET requests and response objects for POST and PATCH operations. This addition streamlines API communication within the application, enhancing modularity and maintainability.

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final String baseUrl = "http://23.106.49.10:3000/";
+  final Dio _dio;
+
+  ApiService({required Dio dio}) : _dio = dio;
+  Future getData({
+    required String endPoint,
+    Map<String, dynamic>? headers,
+  }) async {
+    Response response = await _dio.get(
+      baseUrl + endPoint,
+      options: Options(
+        contentType: "application/json",
+        responseType: ResponseType.json,
+        headers: headers,
+      ),
+    );
+    var jsonData = response.data;
+    return jsonData;
+  }
+
+  Future<Response<Map<String, dynamic>>> postData({
+    required String endPoint,
+    required Map<String, dynamic> data,
+    Map<String, dynamic>? headers,
+  }) async {
+    Response<Map<String, dynamic>> response = await _dio.post(
+      options: Options(
+        headers: headers,
+        contentType: "application/json",
+      ),
+      baseUrl + endPoint,
+      data: data,
+    );
+
+    return response;
+  }
+
+  patchData({
+    required String endPoint,
+    required Map<String, dynamic> data,
+    Map<String, dynamic>? headers,
+  }) async {
+    Response response = await _dio.patch(
+      baseUrl + endPoint,
+      data: data,
+      options: Options(
+        contentType: "application/json",
+        responseType: ResponseType.json,
+        headers: headers,
+      ),
+    );
+
+    return response;
+  }
+}

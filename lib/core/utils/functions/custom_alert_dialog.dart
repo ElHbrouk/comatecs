06cb6f4ef87f0e617b787d346b58b9e9fa4455df
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-09-04 15:52:41 +0300

    Add custom alert dialog utility function
    This commit introduces a new utility function for displaying a custom alert
    dialog in a Flutter application. The function, `customAlertDialog`, allows for
    a flexible and reusable way to show dialogs with custom content. It accepts a
    `BuildContext` and a `Widget` as required parameters, encapsulating the logic
    for displaying the dialog with specified properties such as elevation and
    alignment. This enhancement simplifies the process of showing dialogs across
    the application, promoting code reuse and consistency in UI presentation.

---
+++ lib/core/utils/functions/custom_alert_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> customAlertDialog({
+  required BuildContext context,
+  required Widget child,
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) => Dialog(
+      elevation: 5.0,
+      alignment: Alignment.center,
+      child: child,
+    ),
+  );
+}

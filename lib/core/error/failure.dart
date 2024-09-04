891db23966a8622017bf7d9f56c9df5958f5cdf4
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-09-04 15:51:47 +0300

    Refactor error handling with Failure classes and Dio exceptions
    Introduce a comprehensive error handling mechanism using abstract `Failure` class and its subclass `ServerFailure` to manage different types of failures encountered during API interactions. This includes handling various `DioException` types such as connection timeout, send timeout, receive timeout, bad certificate, bad response, request cancellation, connection error, and unknown errors. Additionally, it provides a factory constructor to handle specific HTTP status codes (400, 401, 403, 500) for detailed error messages. This refactoring enhances the robustness and clarity of error management in the application.

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,1 +1,46 @@
-class Failure{}
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  Failure({required this.message});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.message});
+  factory ServerFailure.fromDioExeption({required DioException dioException}) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(message: 'Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(message: 'Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(message: 'Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(message: 'Bad certificate with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServerFailure.formBadResponse(dioException);
+      case DioExceptionType.cancel:
+        return ServerFailure(message: 'Request to ApiServer was cancelled');
+      case DioExceptionType.connectionError:
+        log(dioException.toString());
+        return ServerFailure(
+            message: 'Connection error with ApiServer $dioException');
+      case DioExceptionType.unknown:
+        return ServerFailure(message: 'Something went wrong');
+    }
+  }
+  factory ServerFailure.formBadResponse(DioException response) {
+    var dioResponse = response.response?.statusCode;
+
+    if (dioResponse == 400 || dioResponse == 401 || dioResponse == 403) {
+      return ServerFailure(message: 'Error, Unauthorized Access');
+    } else if (dioResponse! <= 500) {
+      return ServerFailure(message: 'Error, Internal Server Error');
+    } else {
+      return ServerFailure(message: 'Error, Something went wrong');
+    }
+  }
+}

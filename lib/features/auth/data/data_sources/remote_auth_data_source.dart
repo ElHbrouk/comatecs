55cad50d2b0bd21211d421109cff15685ac7a6cc
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-09-04 15:51:14 +0300

    Add remote authentication data source implementation
    Implement the `RemoteAuthDataSource` interface with methods for creating an account, logging in, resetting password, and forgetting password. Each method interacts with the API service to perform the respective authentication operations. The login method also handles token storage in shared preferences for session management.

---
+++ lib/features/auth/data/data_sources/remote_auth_data_source.dart
@@ -0,0 +1,88 @@
+import 'package:comatecs/constants.dart';
+import 'package:comatecs/core/services/shared_prefrences_singleton.dart';
+import 'package:comatecs/core/utils/api_service.dart';
+import 'package:comatecs/features/auth/domain/entities/user_entity.dart';
+
+abstract class RemoteAuthDataSource {
+  Future<UserEntity> createAccountWithEmailAndPassword({
+    required UserEntity userEntity,
+  });
+  Future<UserEntity> login({
+    required UserEntity userEntity,
+  });
+  Future<UserEntity> resetPassword({
+    required UserEntity userEntity,
+  });
+  Future<UserEntity> forgetPassword();
+}
+
+class RemoteAuthDataSourceImplementaion implements RemoteAuthDataSource {
+  final ApiService apiService;
+
+  RemoteAuthDataSourceImplementaion({required this.apiService});
+  @override
+  Future<UserEntity> createAccountWithEmailAndPassword({
+    required UserEntity userEntity,
+  }) async {
+    await apiService.postData(
+      endPoint: 'users/register',
+      data: {
+        "email": userEntity.email,
+        "phoneNumber": int.parse(userEntity.phoneNumber!),
+        // "name": name.text,
+        "address": userEntity.address,
+        "workType": userEntity.workType,
+        "companyType": userEntity.companyType,
+        "employeesNumber": userEntity.employeeNumber,
+        "jobTitle": userEntity.yourRole,
+      },
+    );
+    return userEntity;
+  }
+
+  @override
+  Future<UserEntity> login({
+    required UserEntity userEntity,
+  }) async {
+    var response = await apiService.postData(
+      headers: {
+        'Authorization':
+            'Bearer ${SharedPrefrencesSingleton.getString(key: kIsTokenGot)}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      endPoint: 'users/login',
+      data: {
+        "email": userEntity.email,
+        "password": userEntity.password,
+      },
+    );
+
+    SharedPrefrencesSingleton.setString(
+      key: kIsTokenGot,
+      value: response.data!['access_token'],
+    );
+
+    return userEntity;
+  }
+
+  @override
+  Future<UserEntity> resetPassword({
+    required UserEntity userEntity,
+  }) async {
+    await apiService.patchData(
+      endPoint: 'users/profile',
+      data: {
+        "password": userEntity.password,
+      },
+    );
+
+    return userEntity;
+  }
+
+  @override
+  Future<UserEntity> forgetPassword() {
+    // TODO: implement forgetPassword
+    throw UnimplementedError();
+  }
+}
